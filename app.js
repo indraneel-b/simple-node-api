4ce26c3f07e93a6be1e90b553c9b546a5f8d42e5
Indraneel Bhakta <indraneelb@zeta.tech>
2021-06-09 00:10:19 +0530

    Init, added CRUD APIs for users

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const db = require('./queries')
+
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+let users = [];
+
+app.route('/users')
+    .get(db.getUsers)
+    .post(db.createUser)
+    .put(db.updateUser)
+    .delete(db.deleteUser)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
